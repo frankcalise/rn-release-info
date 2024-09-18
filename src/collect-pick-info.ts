dade419431302269a0f67fe89205ed51c19fcd5e
Frank Calise <fcalise@gmail.com>
2024-09-17 22:37:46 -0400

    fix: get commit datetime

--- src/collect-pick-info.ts
+++ src/collect-pick-info.ts
@@ -12,169 +12,13 @@
  *    b. Record the hash and datetime
  */
 
+import { parseCommitLinks, parsePullRequestLinks } from "./parsing";
 import {
-  findMergeComment,
-  parseCommitLinks,
-  parsePullRequestLinks,
-} from "./parsing-utils";
-
-// Account for Original and Pick to 0.76 links
-// Can be multiple pick links
-
-async function queryProjectID(targetRelease: string): Promise<string> {
-  const query = `
-    {
-      organization(login: "reactwg") {
-        projectsV2(first: 10) {
-          nodes {
-            id
-            title
-          }
-        }
-      }
-    }
-  `;
-
-  const proc = Bun.spawn(["gh", "api", "graphql", "-f", `query=${query}`]);
-
-  const queryResponse = await new Response(proc.stdout).text();
-  const data = JSON.parse(queryResponse).data;
-
-  // find the project id where the title includes the first two numbers of the target release
-  const project = data.organization.projectsV2.nodes.find((node: any) => {
-    const semVer = targetRelease.split(".");
-    return node.title.includes(`${semVer[0]}.${semVer[1]}`);
-  });
-
-  return project.id;
-}
-
-async function queryProjectInbox({
-  projectID,
-  targetRelease,
-}: {
-  projectID: string;
-  targetRelease: string;
-}) {
-  const query = `
-   {
-      node(id: "${projectID}") {
-        ... on ProjectV2 {
-          items(first: 80, orderBy: {field: POSITION, direction: DESC}) {
-            nodes {
-              id
-              fieldValues(first: 8) {
-                nodes {
-                  ... on ProjectV2ItemFieldTextValue {
-                    text
-                    field {
-                      ... on ProjectV2FieldCommon {
-                        name
-                      }
-                    }
-                  }
-                  ... on ProjectV2ItemFieldDateValue {
-                    date
-                    field {
-                      ... on ProjectV2FieldCommon {
-                        name
-                      }
-                    }
-                  }
-                  ... on ProjectV2ItemFieldSingleSelectValue {
-                    name
-                    field {
-                      ... on ProjectV2FieldCommon {
-                        name
-                      }
-                    }
-                  }
-                }
-              }
-              content {
-                ... on Issue {
-                  number
-                  title
-                  body
-                  createdAt
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `;
-
-  const proc = Bun.spawn(["gh", "api", "graphql", "-f", `query=${query}`]);
-
-  const queryResponse = await new Response(proc.stdout).text();
-  const data = JSON.parse(queryResponse).data.node.items.nodes;
-
-  // Find the issues in group (requested target release) that are in the "Inbox" status
-  const filteredIssues = data.filter((item: any) => {
-    const fieldValues = item.fieldValues.nodes;
-    // filter fieldValues out if it is an empty object
-    const filteredFieldValues = fieldValues.filter(
-      (field: any) => Object.keys(field).length > 0
-    );
-
-    const statusField = filteredFieldValues.find(
-      (field: any) => field?.field.name === "Status"
-    );
-    const targetReleaseField = filteredFieldValues.find(
-      (field: any) => field?.field.name === "Target Release"
-    );
-    const titleField = filteredFieldValues.find(
-      (field: any) => field?.field.name === "Title"
-    );
-
-    if (statusField !== undefined && targetReleaseField !== undefined) {
-      return (
-        statusField.name === "Done / Picked" &&
-        (targetReleaseField.name as string).indexOf(targetRelease) > -1 &&
-        titleField.text.indexOf("Test Report") === -1
-      );
-    }
-    return false;
-  });
-
-  return filteredIssues;
-}
-
-async function queryPullRequest(num: number) {
-  const owner = "facebook";
-  const repo = "react-native";
-
-  const query = `
-    {
-      repository(owner: "${owner}", name: "${repo}") {
-        pullRequest(number: ${num}) {
-          comments(first: 50) {
-            nodes {
-              author {
-                login
-              }
-              body
-              createdAt
-            }
-          }
-        }
-      }
-    }
-  `;
-
-  const proc = Bun.spawn(["gh", "api", "graphql", "-f", `query=${query}`]);
-
-  const output = await new Response(proc.stdout).text();
-  const data = JSON.parse(output).data;
-
-  const mergeInfo = findMergeComment(
-    data.repository.pullRequest.comments.nodes
-  );
-
-  return mergeInfo;
-}
+  queryCommitInfo,
+  queryProjectID,
+  queryProjectInbox,
+  queryPullRequest,
+} from "./queries";
 
 function formatResultLine(commitHash: string, date: string, title: string) {
   return `${commitHash.substring(0, 7)} : ${date} : ${title}`;
@@ -211,6 +55,7 @@ for (const issue of inboxIssues) {
   const issueBody = issue.content.body;
   const issueCreatedAt = issue.content.createdAt;
   const issueURL = issue.content.url;
+
   let flagged = false;
   const output: string[] = [];
 
@@ -229,7 +74,7 @@ for (const issue of inboxIssues) {
         );
       } else {
         flagged = true;
-        itemsToDiscuss.push(`#${issueNumber} - ${issueTitle} (${pullRequest})`);
+        // itemsToDiscuss.push(`#${issueNumber} - ${issueTitle} (${pullRequest})`);
       }
     }
   }
@@ -239,7 +84,10 @@ for (const issue of inboxIssues) {
   if (commitLinks.length > 0) {
     for (const commit of commitLinks) {
       const commitHash = commit.substring(commit.lastIndexOf("/") + 1);
-      output.push(formatResultLine(commitHash, issueCreatedAt, issueTitle));
+      const commitInfo = await queryCommitInfo(commitHash);
+      output.push(
+        formatResultLine(commitHash, commitInfo.committedDate, issueTitle)
+      );
     }
   }
 
